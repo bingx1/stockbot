cf2341237007f56c965a6091d12ffbf6aad39657
bing xu <bingxu.au@gmail.com>
2021-03-18 23:45:06 +1100

    updated mongo adaptor to remove connection logic (now handled by flask-mongoengine)

--- util/mongo_adaptor.py
+++ util/mongo_adaptor.py
@@ -1,40 +1,25 @@
-import mongoengine
 import json
 from util.models import Change, Item, Taskmeta
 
-class Connection:
-    def __enter__(self):
-        self.conn = mongoengine.connect(db="items")
-        print("Connected to mongodb")
-        return self.conn
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.conn.close()
-
-class DbHandler():
-
-    def __init__(self):
-        self.db = "items"
+class MongoAdaptor():
 
     def put_item(self, json_input: json) -> None:
         ''' Puts an item using the json data passed into the database''' 
         new_item = Item.from_json(json_input)
-        with Connection():
-            if not self.exists(new_item.name):
-                new_item.save()
-                print("{} succesfully added. Now tracking {}".format(
-                    new_item.name, new_item.name))
-            else:
-                print("Item already exists in the database. {} is already being tracked by the stock bot".format(
-                    new_item.name))
+        if not self.exists(new_item.name):
+            new_item.save()
+            print("{} succesfully added. Now tracking {}".format(
+                new_item.name, new_item.name))
+        else:
+            print("Item already exists in the database. {} is already being tracked by the stock bot".format(
+                new_item.name))
         return
 
 
     def put_change(self, change_data):
         ''' Change_data = [item.name, item.stock, datetime.now()] '''
         new_change = Change(item_name = change_data[0], restock = change_data[1], timestamp = change_data[2])
-        with Connection():
-            new_change.save()
+        new_change.save()
         return
 
     def exists(self, item_name: str) -> bool:
@@ -44,48 +29,29 @@ class DbHandler():
                 return True
         return False
 
-    def list_tracked_items(self):
-        ''' Returns a list of the items currently in the database as strings''' 
-        output = ""
-        with Connection():
-            for index, item in enumerate(Item.objects):
-                s = '{}. {} \n'.format(index + 1, item.name)
-                output += s
-                print(s)
-        return output
 
     def fetch_items_dict(self, num = None):
         ''' Returns a list of items as python dictionaries'''
-        with Connection():
-            if not num:
-                return [item._data for item in Item.objects]    
-            else:
-                return [item._data for item in Item.objects[:num]]
+        if not num:
+            return [item._data for item in Item.objects]    
+        else:
+            return [item._data for item in Item.objects[:num]]
 
     def fetch_changes_dict(self, num= None):
         ''' Returns a list of changes as python dictionaries'''
-        with Connection():
-            if not num:
-                return [change._data for change in Change.objects().order_by('-timestamp')]
-            else:
-                return [change._data for change in Change.objects().order_by('-timestamp')[:num]]
+        if not num:
+            return [change._data for change in Change.objects().order_by('-timestamp')]
+        else:
+            return [change._data for change in Change.objects().order_by('-timestamp')[:num]]
 
     def fetch_taskresults_dict(self, num):
-        with Connection():
-            return [taskmeta._data for taskmeta in Taskmeta.objects().order_by('-date_done')[:num]]
+        return [taskmeta._data for taskmeta in Taskmeta.objects().order_by('-date_done')[:num]]
 
     def get_items(self):
         ''' Returns a list of all the Item objects in the database'''
-        with Connection():
-            return Item.objects
+        return Item.objects
 
     def get_changes(self):
         ''' Returns a list of all the Item objects in the database'''
-        with Connection():
-            return Change.objects
+        return Change.objects
     
-
-if __name__ == "__main__":
-    db = DbHandler()
-    x = db.fetch_items_dict()
-    print(x)
