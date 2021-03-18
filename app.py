b0189028db925b82540e29f5090735e7f2efb424
bing xu <bingxu.au@gmail.com>
2021-03-18 23:38:16 +1100

    renamed tasks.py to celerinit.py -- makes more sense

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, render_template
 from util.db_handler import DbHandler
-from celeryfile.tasks import make_celery
+from util.db import initialize_db
+from celeryfile.celeryinit import make_celery
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -14,7 +15,7 @@ app.config['MONGODB_SETTINGS'] = {
 
 DbHandler = DbHandler()
 celery = make_celery(app)
-
+initialize_db(app)
 
 @celery.task()
 def update_item_status():
@@ -64,59 +65,7 @@ def parse_and_return(html, item):
         return None
 
 
-@app.route('/index')
-@app.route('/')
-def index():
-    items = DbHandler.fetch_items_dict(2)
-    return render_template('index.html', title='Home', items=items)
 
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    items = DbHandler.fetch_items_dict()
-    if request.method == 'POST':
-        req = request.form
-        print(req)
-        query = req.get('search')
-        query_result = parse_query(query, items)
-        return render_template('search_results.html', items=query_result, query=query)
-    return render_template('search.html')
-
-
-def parse_query(query, items):
-    queries = query.split() 
-    result = []
-    for item in items:
-        for q in queries:
-            if q in item['name'].lower().split():
-                result.append(item)
-    return result
-
-@app.route('/faq')
-def faq():
-    return render_template('faq.html')
-
-@app.route('/items')
-def get_items():
-    all_items = DbHandler.fetch_items_dict()
-    return render_template('items.html', number=len(all_items), items=all_items)
-
-
-@app.route('/dashboard')
-def dashboard():
-    recent_changes = DbHandler.fetch_changes_dict(3)
-    items = DbHandler.fetch_items_dict()
-    in_stock = [item for item in items if item['stock']]
-    last_update = DbHandler.fetch_taskresults_dict(1)
-    date = last_update[0]['date_done']
-    date = datetime(int(date[:4]),int(date[5:7]),int(date[8:10]),int(date[11:13]), int(date[14:16]))
-    time_formatted = date.strftime(r"%A, %b %d %I:%M%p")
-    return render_template('dashboard.html', date = time_formatted, changes=recent_changes, items=in_stock)
 
 
 if __name__ == "__main__":
