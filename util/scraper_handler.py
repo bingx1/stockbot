fb14b5bcc9abbd2e47b77534decc24b3a35112a9
bing xu <bingxu.au@gmail.com>
2021-03-21 01:16:44 +1100

    updated scraper handler to support samsfitness items

--- util/scraper_handler.py
+++ util/scraper_handler.py
@@ -1,5 +1,6 @@
 from .rogue_parser import RogueParser
 from .kabuki_parser import KabukiParser
+from .sams_parser import SamsParser
 ''' The scraper handler is responsible for allocating a particular parser to be used'''
 
 class ScraperHandler():
@@ -7,12 +8,15 @@ class ScraperHandler():
     def __init__(self) -> None:
         self.rogue = RogueParser()
         self.kabuki = KabukiParser()
-    
+        self.sams = SamsParser()
+
     def parse(self, url, soup):
         if 'rogueaustralia' in url:
             res = self.rogue.parse_html(soup)
         elif 'kabuki' in url:
             res = self.kabuki.parse_html(soup)
+        elif 'samsfitness' in url:
+            res = self.sams.parse_html(soup)
         res['url'] = url
         return res
         
