edf333e656494b993bc1dd9a74fc10c56ecaacd4
bing xu <bingxu.au@gmail.com>
2021-03-20 13:45:25 +1100

    removed unit tests from parsers and added sample kabuki payloads

--- util/scraper_handler.py
+++ util/scraper_handler.py
@@ -1,14 +1,23 @@
-from .rogue_parser import RogueParser
+from rogue_parser import RogueParser
+from kabuki_parser import KabukiParser
 ''' The scraper handler is responsible for allocating a particular parser to be used'''
 
 class ScraperHandler():
 
     def __init__(self) -> None:
         self.rogue = RogueParser()
+        self.kabuki = KabukiParser()
     
     def parse(self, url, soup):
         if 'rogueaustralia' in url:
             res =  self.rogue.parse_html(soup)
             res['manufacturer'] = self.rogue.manufacturer
-            return res
+        elif 'kabuki' in url:
+            res = self.kabuki.parse_html(soup)
+            res['manufacturer'] = self.kabuki.manufacturer
+        res['url'] = url
+        return res
+        
+if __name__ == '__main__':
+    items = []
     
