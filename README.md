51fc9d8d1293b27151e28eae64cb039a572277e2
bing xu <bingxu.au@gmail.com>
2021-04-05 01:31:37 +1000

    added a dockerfile (incomplete) and requirements.txt and updated the readme

--- README.md
+++ README.md
@@ -1,12 +1,43 @@
 # Stock Bot
-Backend written in Flask for a stock bot to track gym equipment. 
-Performs scraping of supported items on a scheduled basis (~5 minutes) via Celery.
+Simple web application to track the stock status of gym equipment. 
+Simplifys the experience of building a home gym in 2020-2021!
+Developed in response to the shortage brought upon by COVID-19 and the unreliability of manufacturers' own email restock alerts.
+Items are automatically refreshed in the background every 5 minutes through Celery.
+ 
+## Author
+Bing Xu
 
-## Currently supported manufacturers:
+### Currently supported manufacturers and websites:
 - Rogue
+- Samsfitness (ATX, Ironmaster)
+- Kabuki Strength
 
-## Structure
+## Changelog
+v.1.0: Beta
+- Serving simple bootstrap frontend
+- Supports items from roguefitness.com, samsfitness.com.au and store.kabukistrength.net
+- Celery background refreshing is working as intended
+- App must be run in separate terminals .... not yet containerized
+
+
+# TODO
+1. Dockerize the backend
+2. Implemenet exception handling for scraping/refreshing of stock status
+3. Containerize entire application (write a docker-compose)
+4. Move front-end to react
+
+## Quickstart
+- Clone this repository and install docker.
+- Run redis and mongodb in a container (or locally)
+- Start the flask backend
+- Run the celery workers and celery beat scheduler
+- Visit http://127.0.0.1:5000/ to see the app running!
+
+## Project Structure
 1. Flask (backend) 
 2. MongoDB (db)
 3. Redis (broker)
 4. Celery (worker)
+
+
+
--- util/sams_parser.py
+++ util/sams_parser.py
@@ -1,6 +1,3 @@
-import requests
-from bs4 import BeautifulSoup
-
 class SamsParser():
     def parse_html(self, soup):
         item = {}
@@ -22,28 +19,3 @@ class SamsParser():
         string = price_str.replace('$', '')
         string = string.replace(',','')
         return float(string)
-
-SamsParser = SamsParser()
-urls = ['https://samsfitness.com.au/racks-cages-stands-smiths/atx-commercial-power-racks/atx-prx-830-power-cage',
-        'https://samsfitness.com.au/racks-cages-stands-smiths/atx-commercial-power-racks/atx-competition-combo-rack',
-        'https://samsfitness.com.au/racks-cages-stands-smiths/atx-commercial-power-racks/atx-prx-840-power-cage',
-        'https://samsfitness.com.au/racks-cages-stands-smiths/atx-commercial-power-racks?product_id=1833',
-        'https://samsfitness.com.au/racks-cages-stands-smiths/atx-commercial-power-racks/atx-prx-820-power-cage',
-        'https://samsfitness.com.au/racks-cages-stands-smiths/atx-commercial-power-racks/atx-prx-810-power-cage',
-        'https://samsfitness.com.au/racks-cages-stands-smiths/atx-commercial-power-racks/atx-pulldown-seat',
-        'https://samsfitness.com.au/racks-cages-stands-smiths/atx-commercial-power-racks/atx-prx-800-lat-tower-option-125kg',
-        'https://samsfitness.com.au/racks-cages-stands-smiths/atx-commercial-power-racks/atx-hrx-810',
-        'https://samsfitness.com.au/dumbbells/ironmaster-quick-lock-dumbbell-system/ironmaster-quick-lock-dumbbells-base-kit',
-        'https://samsfitness.com.au/ironmaster/quick-lock-dumbbell-165lb-kit',
-        'https://samsfitness.com.au/ironmaster/ironmaster-quick-lock-add-on-kit',
-        'https://samsfitness.com.au/barbells/elite-olympic-bars/atx-ram-power-bar']
-items = []
-for url in urls:
-    req = requests.get(url)
-    soup = BeautifulSoup(req.text, 'html.parser')
-    item = SamsParser.parse_html(soup)
-    item['url'] = url
-    items.append(item)
-    # print(item)
-
-print(items)
